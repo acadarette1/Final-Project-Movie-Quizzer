25e82f1143d99bad021004c925bd0f4d6cd7ab51
Aaron Cadarette <aaroncat567@gmail.com>
2021-04-21 02:01:53 -0400

    I need help

---
+++ Final Project Movie Quizzer/frmTrivia.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace Final_Project_Movie_Quizzer
+{
+    partial class frmTrivia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdoTrue = new System.Windows.Forms.RadioButton();
+            this.rdoFalse = new System.Windows.Forms.RadioButton();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.txtTrueFalse = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rdoTrue
+            // 
+            this.rdoTrue.AutoSize = true;
+            this.rdoTrue.Location = new System.Drawing.Point(357, 92);
+            this.rdoTrue.Name = "rdoTrue";
+            this.rdoTrue.Size = new System.Drawing.Size(47, 17);
+            this.rdoTrue.TabIndex = 0;
+            this.rdoTrue.TabStop = true;
+            this.rdoTrue.Text = "True";
+            this.rdoTrue.UseVisualStyleBackColor = true;
+            // 
+            // rdoFalse
+            // 
+            this.rdoFalse.AutoSize = true;
+            this.rdoFalse.Location = new System.Drawing.Point(357, 137);
+            this.rdoFalse.Name = "rdoFalse";
+            this.rdoFalse.Size = new System.Drawing.Size(50, 17);
+            this.rdoFalse.TabIndex = 1;
+            this.rdoFalse.TabStop = true;
+            this.rdoFalse.Text = "False";
+            this.rdoFalse.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(301, 273);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(25, 78);
+            this.txtQuestion.Multiline = true;
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.Size = new System.Drawing.Size(241, 126);
+            this.txtQuestion.TabIndex = 3;
+            // 
+            // txtTrueFalse
+            // 
+            this.txtTrueFalse.Location = new System.Drawing.Point(36, 261);
+            this.txtTrueFalse.Name = "txtTrueFalse";
+            this.txtTrueFalse.Size = new System.Drawing.Size(168, 20);
+            this.txtTrueFalse.TabIndex = 4;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(403, 273);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Movie Trivia Questions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Correct/Incorrect";
+            // 
+            // frmTrivia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 377);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtTrueFalse);
+            this.Controls.Add(this.txtQuestion);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.rdoFalse);
+            this.Controls.Add(this.rdoTrue);
+            this.Name = "frmTrivia";
+            this.Text = "Movie Trivia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdoTrue;
+        private System.Windows.Forms.RadioButton rdoFalse;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.TextBox txtTrueFalse;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Final Project Movie Quizzer/frmTrivia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project_Movie_Quizzer
+{
+    public partial class frmTrivia : Form
+    {
+        public frmTrivia()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
